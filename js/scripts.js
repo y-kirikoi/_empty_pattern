9ed4b699f5962fcc6e64ec99d4d33dda24cca565
y-kirikoi <kirikoy1@gmail.com>
2018-02-02 00:27:23 +0200

    +

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+	$('.slider-wrapper').slick({
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	arrows: false,
+	dots: true,
+	dotsClass: 'slider-dots',
+	autoplay: true,
+	autoplaySpeed: 2000
+	});
+
+});
